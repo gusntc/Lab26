5d1d68b52970d4a02c032dba6c6c4cdece223469
gusntc <Natacha_rungbanpant.ac.th>
2022-02-18 19:00:15 +0700

    EditLab26_1

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -64,3 +64,47 @@ void List::append(int d){
 }
 
 //Write List::remove() here
+void List::remove(int idx){
+	Node* current = root;
+	if (idx == 0) {
+        root = root->next;
+        delete current;
+    }else{
+    	for (int i = 0; i < idx-1; i++) {
+        current = current->next;
+		}
+		Node* p = current->next;
+		current->next = current->next->next;
+		delete p;
+    }
+}
+
+
+
+int main(){
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n";
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n";
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
+	return 0;
+}
